f8b45245de8c44fe0c3e91f2e221032fc9f3b755
Nuwan Dewapriya <67752825+nuwan-d@users.noreply.github.com>
2022-04-18 19:00:39 -0400

    Add files via upload

---
+++ VASP_soft_domain/sub_ced.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=YOUR ACCOUNT NAME
+#SBATCH --nodes=15                 # Number of nodes
+#SBATCH --ntasks-per-node=32      # Number of MPI process
+#SBATCH --mem=0                # memory per node
+#SBATCH --time=0-11:55            # time (DD-HH:MM)
+#SBATCH --output=vasp_out_%j.txt
+#SBATCH --mail-user=YOUR-EMAIL
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=s_0.9T_400
+
+module load nixpkgs/16.09  intel/2019.3  intelmpi/2019.3.199 vasp/6.1.0
+
+cd $SLURM_SUBMIT_DIR
+
+srun vasp_std
+
